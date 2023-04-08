fa15a9c80d171f043b9faaf6a6db2131ce05c7c5
Kamal Gupta <agdigitalsmith@gmail.com>
2023-04-08 19:28:13 +0530

    add

---
+++ Backend/routes/join.routes.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const { joinValidator } = require("../Middlewares/join.middleware.js")
+const { RoomModel } = require("../models/Room.Model")
+
+const joinRouter = express.Router()
+
+
+joinRouter.patch("/:id",joinValidator,async(req,res)=>{
+    const {id} = req.params
+    const {key} = req.headers
+    let newPlayer = {
+        [key]:JSON.stringify(req.body)
+    }
+    console.log(newPlayer)
+ try {
+    await RoomModel.updateOne({rid:id},newPlayer)
+    res.send({
+        message:"Player joined",
+        status:1,
+        error:false
+    })
+ } catch (error) {
+
+    res.send({
+        message:"Failed: "+error.message,
+        status:0,
+        error:true
+    })
+    
+ }
+   
+
+})
+
+module.exports ={
+    joinRouter
+}
